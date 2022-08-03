f27051c09f3e08a17ada0f53e937df51dced0999
Hiteek <ruben.ricapa.c@uni.pe>
2022-08-03 14:38:13 -0500

    Flashcard added

--- src/main/java/Flashcard.java
+++ src/main/java/Flashcard.java
@@ -1,5 +1,42 @@
 import java.util.Objects;
 
+interface IFlashcard{
+    void setQuestion(Question q);
+    void setAnswer(Answer a);
+}
+class WordFlashcard implements IFlashcard{
+    private Question question;
+    private Answer answer;
+    @Override
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    @Override
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
+    }
+}
+class mcFlashcard implements IFlashcard{
+
+    private Question question;
+    private Answer answer;
+    @Override
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    @Override
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
+    }
+}
+
+class Director{
+    public void constructMCFlashcard(IFlashcard iflashcard){
+
+    }
+}
 public class Flashcard {
     private Question question;
     private Answer answer;
@@ -7,7 +44,14 @@ public class Flashcard {
         this.question = question;
         this.answer = answer;
     }
+    public Answer getAnswer() {
+        return answer;
+    }
+    public Question getQuestion() {
+        return question;
+    }
     public boolean isCorrect(String answer){
         return Objects.equals(answer, this.answer.getCorrectAnswer());
     }
 }
+
