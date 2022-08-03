a84a06fd9cef83b7a4a39cb8ed6aa2441e42c0c6
JDomCamZ <juansehermanito@gmail.com>
2022-08-03 12:54:18 -0500

    Setting multiplechoices.

--- src/main/java/MultipleChoices.java
+++ src/main/java/MultipleChoices.java
@@ -3,19 +3,17 @@ import java.util.ArrayList;
 public class MultipleChoices implements Answer{
     private String CorrectAnswer;
     private ArrayList<String> IncorrectAnswers;
-    private String[] Answers;
-    public void MultipleChoices(String correctAnswers, ArrayList<String> incorrectAnswers){
+    private ArrayList<String> Answers;
+    public MultipleChoices(String correctAnswers, ArrayList<String> incorrectAnswers){
         this.CorrectAnswer = correctAnswers;
         this.IncorrectAnswers = incorrectAnswers;
     }
     @Override
     public void setAnswer(){
-        this.Answers = new String[1+ this.IncorrectAnswers.length];
-        System.arraycopy(this.CorrectAnswer, 0, this.Answers, 0, 1);
-        System.arraycopy(this.IncorrectAnswers, 0, this.Answers, 1, this.IncorrectAnswers.length);
+        this.Answers = new ArrayList<>();
     }
     @Override
     public String getAnswer(){
-
+        return this.CorrectAnswer;
     }
 }
