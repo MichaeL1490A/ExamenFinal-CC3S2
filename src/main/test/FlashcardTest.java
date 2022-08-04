c120b243b2d0b541f02f5ec4724e51011b9c0a1a
JDomCamZ <juansehermanito@gmail.com>
2022-08-03 21:46:57 -0500

    Generating 2nd test.

--- src/main/test/FlashcardTest.java
+++ src/main/test/FlashcardTest.java
@@ -4,10 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FlashcardTest {
 
+    private Flashcard flash;
+
     @DisplayName("Dado que creamos un Word Flashcard")
     @Nested
     class WordFlashcardTest{
-        private Flashcard flash;
 
         @BeforeEach
         void setUp(){
@@ -22,13 +23,39 @@ public class FlashcardTest {
         }
 
         @Test
-        @DisplayName("Confirmacion de funcionamiento de strings")
+        @DisplayName("Al crear el Flashcard podremos asignar su pregunta y respuesta inicial")
         public void testWordFlashcardTerms() {
-            assertEquals("Amogus",flash.getQuestion().toString());
-            assertEquals("SUS", flash.getAnswer().toString());
+            assertAll("Verifica que todos los terminos insertados en el Flashcard son correctamente creados",
+                    () -> assertEquals("Amogus",flash.getQuestion().toString()),
+                    () -> assertEquals("SUS", flash.getAnswer().toString())
+            );
+        }
+    }
+
+    @DisplayName("Dado que creamos un Word Flashcard")
+    @Nested
+    class MCFlashcardTest{
+
+        @BeforeEach
+        void setUp(){
+            FlashcardCreator flashy = new FlashcardCreator();
+            WordFlashcardBuilder wordy = new WordFlashcardBuilder();
+            flashy.constructWordFlashcard(wordy);
+            Question question = new Question("Amogus");
+            Answer answer = new Word("SUS");
+            wordy.setQuestion(question);
+            wordy.setAnswer(answer);
+            flash = wordy.getResult();
         }
 
-
+        @Test
+        @DisplayName("Al crear el Flashcard podremos asignar su pregunta y respuesta inicial")
+        public void testMCFlashcardTerms() {
+            assertAll("Verifica que todos los terminos insertados en el Flashcard son correctamente creados",
+                    () -> assertEquals("Amogus",flash.getQuestion().toString()),
+                    () -> assertEquals("SUS", flash.getAnswer().toString())
+            );
+        }
     }
 
 }
