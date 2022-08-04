390a12bb61c7132888424f02c85a253811c7ff5b
JDomCamZ <juansehermanito@gmail.com>
2022-08-03 22:31:23 -0500

    Trying 2nd test.

--- src/main/test/FlashcardTest.java
+++ src/main/test/FlashcardTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FlashcardTest {
@@ -32,28 +34,39 @@ public class FlashcardTest {
         }
     }
 
-    @DisplayName("Dado que creamos un Word Flashcard")
+    @DisplayName("Dado que creamos un MC Flashcard")
     @Nested
     class MCFlashcardTest{
 
         @BeforeEach
         void setUp(){
             FlashcardCreator flashy = new FlashcardCreator();
-            WordFlashcardBuilder wordy = new WordFlashcardBuilder();
-            flashy.constructWordFlashcard(wordy);
+            MCFlashcardBuilder mcfly = new MCFlashcardBuilder();
+            flashy.constructMCFlashcard(mcfly);
             Question question = new Question("Amogus");
-            Answer answer = new Word("SUS");
-            wordy.setQuestion(question);
-            wordy.setAnswer(answer);
-            flash = wordy.getResult();
+            String correct = "1";
+            ArrayList<String> incorrect = new ArrayList<>();
+            incorrect.add("2");
+            incorrect.add("3");
+            incorrect.add("4");
+            MultipleChoices answer = new MultipleChoices(correct, incorrect);
+            mcfly.setQuestion(question);
+            mcfly.setAnswer(answer);
+            flash = mcfly.getResult();
         }
 
         @Test
         @DisplayName("Al crear el Flashcard podremos asignar su pregunta y respuesta inicial")
         public void testMCFlashcardTerms() {
+            ArrayList<String> inc = new ArrayList<>();
+            inc.add("2");
+            inc.add("3");
+            inc.add("4");
+            System.out.println(flash.getAnswer().toString());
             assertAll("Verifica que todos los terminos insertados en el Flashcard son correctamente creados",
                     () -> assertEquals("Amogus",flash.getQuestion().toString()),
-                    () -> assertEquals("SUS", flash.getAnswer().toString())
+                    () -> assertEquals("1", flash.getAnswer().getCorrectAnswer())
+               //     () -> assertEquals(inc, flash.getAnswer().getIncorect)
             );
         }
     }
